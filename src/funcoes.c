e4647d511eb57908b6274344c66e2f9466a3a6d5
Felipe Jun Kumada <felipekumada@outlook.com>
2023-05-28 22:33:54 -0300

    Adicionado o comando JMP

--- src/funcoes.c
+++ src/funcoes.c
@@ -22,18 +22,20 @@
 #define JZ 160
 
 
-void checkByte(uint8_t buffer [],uint8_t *Ac, uint8_t *Pc,int *tamanho){
+void checkByte(uint8_t buffer [],uint8_t *Ac,int *tamanho){
 	//{ 0,1,2, 3, 4, 5, 6, 7,8, 8,10,11}
 	//{42,0,0,32,10,48,11,16,1,240,3,5}
 	uint8_t indexAtual = *tamanho;
 	uint8_t proxIndex = (++(indexAtual));
 	switch(buffer[*tamanho]){
 		case HLT:
-			printf("Resultado final:\nAC: %d\nPC: %d",*Ac,*Pc);
+			printf("Resultado final:\nAC: %d\nPC: %d",buffer[1],buffer[2]);
 			exit(0);
 		break;
 		case STA:
 			printf("STA");
+			buffer[buffer[proxIndex]] = *Ac;
+			(*tamanho)++;
 		break;
 		case LDA:
 			printf("LDA");
@@ -57,6 +59,7 @@ void checkByte(uint8_t buffer [],uint8_t *Ac, uint8_t *Pc,int *tamanho){
 		break;
 		case JMP:
 			printf("JMP");
+			(*tamanho) = buffer[proxIndex];
 		break;
 		case JN:
 			printf("JN");
@@ -72,7 +75,7 @@ void checkByte(uint8_t buffer [],uint8_t *Ac, uint8_t *Pc,int *tamanho){
 int binary(FILE *file){
 	
 	uint8_t buffer [MAX_SIZE];
-	uint8_t AC, PC = 0;
+	uint8_t AC = 0;
 	int i = 0;
 	fseek(file, 0, SEEK_SET);
 	fread(&buffer, MAX_SIZE, 1, file);
@@ -82,10 +85,9 @@ int binary(FILE *file){
 	}
 	else{
 		AC = buffer[1];
-		PC = buffer[2];
 		while(1){
-			PC = i;
-			checkByte(buffer,&AC,&PC,&i);
+			buffer[2] = i;
+			checkByte(buffer,&AC,&i);
 		}
 	}
 	return 0;
